785a87d6da200087292f52fb5a0bf084cad7252e
ngoldfarb <nagoldfarb@wpi.edu>
2017-11-13 14:00:21 -0500

    Gravity matrix function created

--- Dynamics.py
+++ Dynamics.py
@@ -1,5 +1,8 @@
 from math import sin as s
 from math import cos as c
+import numpy as np
+
+
 class RoboticArm(object):
 
     def __init__(self):
@@ -28,15 +31,37 @@ class RoboticArm(object):
         M_23 = self.link_inertia[3][1] + self.link_masses[3]*self.link_centroid[2]**2 + \
                self.link_masses[3]*self.link_lengths[1]*self.link_centroid[2]*c(theta_3)
 
-        M_23 = 0
 
+        M_31 = 0
         M_32 =  self.link_inertia[3][1] + self.link_masses*self.link_centroid[2]**2 + \
                 self.link_masses[3]*self.link_lengths[2]*self.link_centroid[2]*c(theta_3)
         M_33 = self.link_inertia[3][1] + self.link_masses[3]*self.link_centroid[2]**2
 
 
-        pass
-    def make_gravity_matrix(self):
+
+        M = np.matrix([ [ M_11, M_12, M_13],
+                        [ M_21, M_22, M_23],
+                        [ M_31, M_32, M_33] ] )
+        return  M
+
+    def make_gravity_matrix(self,theta_1,theta_2,theta_3):
+
+
+        gravity = 9.81
+
+        G_1 = 0
+
+
+        G_2 = -(self.link_masses[2]*self.link_centroid[1] + self.link_masses[3]*self.link_lengths[1])*gravity*c(theta_2) \
+              -self.link_masses[3]*self.link_centroid[2]*c(theta_2+theta_3)
+
+        G_3 = -self.link_masses[3]*gravity*self.link_centroid[2]*c(theta_2+theta_3)
+
+        G = np.matrix[ [ G_1 ], [ G_2 ], [ G_3 ] ]
+
+
+        return G
+
         pass
     def make_coriolis_matrix(self):
         pass
